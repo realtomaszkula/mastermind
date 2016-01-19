0c9392f5491ee717b80617525ed21f8ebe906d82
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-19 12:14:14 +0100

    Added major classes

--- game.rb
+++ game.rb
@@ -1,17 +1,38 @@
-board = %{
-			  c   o 
- 1| | | | | -| |-| |
- 2| | | | | -| |-| |
- 3| | | | | -| |-| |
- 4| | | | | -| |-| |
- 5| | | | | -| |-| |
- 6| | | | | -| |-| |
- 7| | | | | -| |-| |
- 8| | | | | -| |-| |
- 9| | | | | -| |-| |
-10| | | | | -| |-| |
+class MasterMind
 
-}
+	def initialize(name)
+		@player = Player.new(name)
+		@board = Board.new
+		play
+	end	
+
+	Player = Struct.new(:name)	
+
+	class Board
+		attr_accessor :guess
+		attr_reader :board
+
+		def initialize
+			@board = draw_board
+			@guess = draw_guess
+		end
 
 
-puts board
+		def draw_board	
+			@board = %{\n              c   o\nx|#{g[0]}|#{g[1]}|#{g[2]}|#{g[3]}|-| |-| |}
+		end
+
+		def draw_guess
+			@guess = [" "," "," "," "]
+		end
+	end
+end
+
+
+
+
+puts %{*** Hello, welcome to NUMBERWANG ***
+ENTER PLAYER NAME:}
+
+plr = gets.chomp
+MasterMind.new(plr)
