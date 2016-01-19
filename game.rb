31778bc3ef3dd258475d8239a83f61e5edd0113f
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-19 12:35:52 +0100

    AI class

--- game.rb
+++ game.rb
@@ -3,32 +3,49 @@ class MasterMind
 	def initialize(name)
 		@player = Player.new(name)
 		@board = Board.new
+		@AI = AI.new
 		play
 	end	
 
 	Player = Struct.new(:name)	
 
+	class AI
+		attr_reader :numberwang
+		def initialize
+			@numberwang = [rand(1..6),rand(1..6),rand(1..6),rand(1..6)]
+		end
+	end
+
 	class Board
-		attr_accessor :guess
 		attr_reader :board
+		attr_writer :guess
 
 		def initialize
-			@board = draw_board
 			@guess = draw_guess
+			@board = draw_board
 		end
 
+		def view_board
+			puts @board
+		end
 
-		def draw_board	
-			@board = %{\n              c   o\nx|#{g[0]}|#{g[1]}|#{g[2]}|#{g[3]}|-| |-| |}
+		private
+
+		def draw_board
+			@board = %{\n            c   o\nx|#{@guess[0]}|#{@guess[1]}|#{@guess[2]}|#{@guess[3]}|-| |-| |}
 		end
 
 		def draw_guess
 			@guess = [" "," "," "," "]
 		end
 	end
+
+	def play
+		@board.view_board
+		@AI.numberwang
+	end
+
 end
-
-
 
 
 puts %{*** Hello, welcome to NUMBERWANG ***
